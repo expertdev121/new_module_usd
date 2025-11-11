a84319cc55d43b7a59b8f57793bbbb722e2c6095
vishal <jhavishal523@gmail.com>
2025-11-11 16:46:47 +0530

    .

--- scripts/seed-historical-donations.ts
+++ scripts/seed-historical-donations.ts
@@ -2,7 +2,7 @@
 import 'dotenv/config';
 import Papa from 'papaparse';
 
-process.env.DATABASE_URL = 'postgresql://levhatora_final_owner:npg_FmBlvp78SNqZ@ep-sweet-shadow-a95u1c5c-pooler.gwc.azure.neon.tech/levhatora_final?sslmode=require&channel_binding=require'
+process.env.DATABASE_URL = 'postgresql://levhatora_final_owner:npg_FmBlvp78SNqZ@ep-tiny-fog-a9fqoj3f-pooler.gwc.azure.neon.tech/levhatora_final?sslmode=require&channel_binding=require'
 
 import { db } from '@/lib/db';
 import {
